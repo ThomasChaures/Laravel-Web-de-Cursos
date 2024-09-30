0e2443992bd901827428c016d3e1e76e833f59e0
ThomasChaures <chauresthomas12@gmail.com>
2024-09-30 19:41:05 -0300

    Login/Logout/Register de usuarios.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,16 +1,74 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function index(){
+    public function login()
+    {
         return view('auth.login');
     }
 
-    public function authenticate(Request $request){
-        
+    public function register(){
+        return view('auth.register');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()
+            ->route('auth.login')
+            ->with('feedback', ['messages' => ['Sesión cerrada']]);
+    }
+
+    public function authenticate(Request $request)
+    {
+        // Validar los datos de entrada
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // Obtener las credenciales
+        $credentials = $request->only('email', 'password');
+
+        // Intentar autenticar
+        if (Auth::attempt($credentials)) {
+            return redirect()
+                ->intended('home')
+                ->with('feedback', ['messages' => ['Sesión iniciada con éxito.']]);
+        }
+        return redirect()
+            ->back()
+            ->withInput()
+            ->with('feedback', ['errors' => ['Credenciales incorrectas']]);
+    }
+
+    public function newAccount(Request $request){
+        $request->validate([
+            'name'=> 'required|string|max:100',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Auth::login($user);
+
+        return redirect()
+            ->route('home')
+            ->with('feedback', ['messages' => ['Cuenta creada con exito.']]);
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,23 @@
-<div>
-    <!-- An unexamined life is not worth living. - Socrates -->
-</div>
+@extends('layout.app')
+   @section('title', 'Iniciar-sesion')
+
+
+    @section('content')
+
+
+    <h1>Iniciar Sesion</h1>
+
+    <form action="{{ route('auth.authenticate')}}" method="post">
+        @csrf 
+        <div class="mb-3" >
+            <label for="email"> Email </label>
+            <input type="email" name="email" id="email" class="form-control">
+        </div>  
+        <div class="mb-3">
+            <label for="password"> Password </label>
+            <input type="password" name="password" id="password" class="form-control">
+        </div>  
+        <button type="submit" class="btn btn-primary">Ingresar</button>
+    </form>
+
+    @endsection
