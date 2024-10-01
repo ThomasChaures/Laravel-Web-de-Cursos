f8c3fa4450b623910a5ab7ae724195a74560e8d2
talito2000 <fedetaleb@gmail.com>
2024-10-01 10:38:43 -0300

    Migraciones y base de datos, falta configurar modelos.

---
+++ database/migrations/2024_10_01_124941_create_servicios_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('servicios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 45);
+            $table->string('descripcion', 255);
+            $table->string('img', 45);
+            $table->integer('precio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('servicios');
+    }
+};
