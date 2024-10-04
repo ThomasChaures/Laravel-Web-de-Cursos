50dce7f9d7422f235623ab7adfb3b19eb5b990bf
ThomasChaures <chauresthomas12@gmail.com>
2024-10-04 12:14:04 -0300

    Middleware, cursos, novedades

--- resources/views/cursos.blade.php
+++ resources/views/cursos.blade.php
@@ -1,3 +1,23 @@
-<div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
-</div>
+@extends('layout.app')
+@section('title', 'Cursos')
+
+
+
+@section('content')
+
+<section class="pt-5 pb-5">
+<div class="mx-auto container grid grid-cols-1 md:grid-cols-3 gap-6">
+         @foreach($servicios as $servicio)
+         <div class="bg-white rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105">
+             <img src="{{ asset('uploads/' . $servicio->img) }}" alt="Diseño Web" class="w-full h-40 object-cover">
+             <div class="p-6">
+                 <span class="inline-block text-white text-sm px-3 py-1 rounded-full" style="background: linear-gradient(90deg, #3b82f6 0%, #22c55e 100%);">{{$servicio->categoria}}</span>
+                 <h3 class="text-xl font-semibold mt-4">{{$servicio->nombre}}</h3>
+                 <p class="text-gray-600 mt-2">{{$servicio->clases}} Clases | {{$servicio->estudiantes}} Estudiantes</p>
+                 <p class="text-lg font-bold mt-4">${{$servicio->precio}}</p>
+             </div>
+         </div>
+     @endforeach
+ </div> 
+</section> 
+@endsection
