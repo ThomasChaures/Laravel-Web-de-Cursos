98938dffad73765db477cd08fef8bc3005146050
unknown <thomas.chaures@davinci.edu.ar>
2025-03-02 18:38:59 -0300

    mercado pago init

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Felatho - @yield('title')</title>  
     @vite('resources/css/app.css')
     <script src="//unpkg.com/alpinejs" defer></script>
